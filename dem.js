8a186e3ef6b56aa3156f372fa0cb2fb647c099e9
Fehzan123 <mohdfehzan1998@gmail.com>
2023-04-26 19:05:16 +0530

    Now go head and add HEllo word before Item Lister Now go head and add HEllo word before Item 2

--- dem.js
+++ dem.js
@@ -17,14 +17,17 @@ console.log(newDiv);
 
 newDiv.setAttribute('title','hello div');
 
+
 // creatTextNode
 var newDivTex=document.createTextNode('hello world');
 // appendChild
 newDiv.appendChild(newDivTex);
+
 // firstElementChild
 console.log(itemList.firstElementChild);
 // firstChild
 console.log(itemList.firstChild);
+
 // nextSibling
 console.log(itemList.nextSibling);
 // nextElementSibling
@@ -35,6 +38,15 @@ console.log(itemList.previousSibling);
 console.log(itemList.previousElementSibling);
 // createElement
 console.log(newDiv.createElement);
+// Now go head and add HEllo word before Item Lister
+// Now go head and add HEllo word before Item 1
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+
+
+console.log(newDiv);
+container.insertBefore(newDiv,h1);
+
 
 
 
