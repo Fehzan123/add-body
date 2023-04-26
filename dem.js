f279c4da60644f0c5b76eaedf85f950f28ba9c63
Fehzan123 <mohdfehzan1998@gmail.com>
2023-04-26 17:26:31 +0530

    used to manipulate the DOM

--- dem.js
+++ dem.js
@@ -1,1 +1,41 @@
-console.log('ggggg');
+
+
+var itemList=document.querySelector("#items");
+// parentElement
+console.log(itemList.parentElement);
+// lastchild
+console.log(itemList.lastElementChild);
+// parentElement
+console.log(itemList.lastChild);
+
+// creat div
+var newDiv=document.createElement('div');
+// creatChild
+console.log(newDiv);
+
+// setAttribute
+
+newDiv.setAttribute('title','hello div');
+
+// creatTextNode
+var newDivTex=document.createTextNode('hello world');
+// appendChild
+newDiv.appendChild(newDivTex);
+// firstElementChild
+console.log(itemList.firstElementChild);
+// firstChild
+console.log(itemList.firstChild);
+// nextSibling
+console.log(itemList.nextSibling);
+// nextElementSibling
+console.log(itemList.nextElementSibling);
+// previousSibling
+console.log(itemList.previousSibling);
+// previousElementSibling
+console.log(itemList.previousElementSibling);
+// createElement
+console.log(newDiv.createElement);
+
+
+
+
