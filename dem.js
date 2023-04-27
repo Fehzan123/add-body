02c8af9a9fe39dce4f2ba4b969ca9c6d0b8c70b6
Fehzan123 <mohdfehzan1998@gmail.com>
2023-04-27 07:53:20 +0530

    first comit

--- dem.js
+++ dem.js
@@ -1,53 +1,16 @@
+var form = document.getElementById('addForm');
+var itemList=document.getElementById('items');
+form.addEventListener('submit',addItem);
 
-
-var itemList=document.querySelector("#items");
-// parentElement
-console.log(itemList.parentElement);
-// lastchild
-console.log(itemList.lastElementChild);
-// parentElement
-console.log(itemList.lastChild);
-
-// creat div
-var newDiv=document.createElement('div');
-// creatChild
-console.log(newDiv);
-
-// setAttribute
-
-newDiv.setAttribute('title','hello div');
-
-
-// creatTextNode
-var newDivTex=document.createTextNode('hello world');
-// appendChild
-newDiv.appendChild(newDivTex);
-
-// firstElementChild
-console.log(itemList.firstElementChild);
-// firstChild
-console.log(itemList.firstChild);
-
-// nextSibling
-console.log(itemList.nextSibling);
-// nextElementSibling
-console.log(itemList.nextElementSibling);
-// previousSibling
-console.log(itemList.previousSibling);
-// previousElementSibling
-console.log(itemList.previousElementSibling);
-// createElement
-console.log(newDiv.createElement);
-// Now go head and add HEllo word before Item Lister
-// Now go head and add HEllo word before Item 1
-var container=document.querySelector('header .container');
-var h1=document.querySelector('header h1');
-
-
-console.log(newDiv);
-container.insertBefore(newDiv,h1);
-
-
+function addItem(e){
+   
+    e.preventDefault();
+    var newItem=document.getElementById('item').value;
+    var li=document.createElement('li');
+    li.className='list-group-item';
+    li.appendChild(document.createTextNode(newItem));
+    itemList.appendChild(li);
+}
 
 
 
