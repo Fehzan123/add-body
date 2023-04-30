f33bc46c0ff7e46c79278fc372b028100e7b6967
Fehzan123 <mohdfehzan1998@gmail.com>
2023-04-30 13:07:57 +0530

    add data in body and lacalStorage

--- apoiment.js
+++ apoiment.js
@@ -4,6 +4,11 @@ function addItems(e){
     e.preventDefault();
     var newItem=document.getElementById('username').value;
     var newItem2=document.getElementById('username2').value;
-   localStorage.setItem('name : '+newItem,'Email : '+newItem2);
+    var newItem3=document.getElementById('phone-no').value;
+   localStorage.setItem(newItem2,' Name '+newItem+','+' Email : '+newItem2+','+' phon no '+newItem3);
+    var li=document.createElement('li');
+li.className='list';
+li.appendChild(document.createTextNode('Name '+newItem+','+' Email : '+newItem2+','+' phone No '+newItem3));
+form.appendChild(li);
 
 }
